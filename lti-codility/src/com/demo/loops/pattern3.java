3221d1b0e77140d98edb9d83b74ce02e4db3b598
Apoorva Bidyanta <apoorva.bid12@gmail.com>
2020-12-18 21:42:56 +0530

    added hello message

--- lti-codility/src/com/demo/loops/pattern3.java
+++ lti-codility/src/com/demo/loops/pattern3.java
@@ -11,6 +11,7 @@ public class pattern3 {
 	public static void main(String[] args) {
 		 int n=5;
 		 //hello
+		 //hello
 		 for(int i=1;i<=5;i++) {
 			 for (int j=1;j<=i;j++) {
 				 System.out.print(i);
