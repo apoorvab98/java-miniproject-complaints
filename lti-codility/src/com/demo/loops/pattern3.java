97806c6dc6c2685239c907cc4d076b965361ce09
Apoorva Bidyanta <apoorva.bid12@gmail.com>
2020-12-18 10:13:41 +0530

    my second commit

--- lti-codility/src/com/demo/loops/pattern3.java
+++ lti-codility/src/com/demo/loops/pattern3.java
@@ -10,6 +10,7 @@ package com.demo.loops;
 public class pattern3 {
 	public static void main(String[] args) {
 		 int n=5;
+		 //hello
 		 for(int i=1;i<=5;i++) {
 			 for (int j=1;j<=i;j++) {
 				 System.out.print(i);
